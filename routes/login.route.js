18385a902c348811a82f0514d74e9129da82c35e
ImDebabrata <debabratakgt@hotmail.com>
2023-01-28 02:06:55 +0530

    login functionallity

--- routes/login.route.js
+++ routes/login.route.js
@@ -1,13 +1,32 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
 const { UserModel } = require("../models/User.model");
 const loginRouter = express.Router();
 
-loginRouter.post("/", (req, res) => {
+loginRouter.post("/", async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
     res.status(401).send({ res: "Please input Email and Password" });
   }
-  res.send("Login Route");
+  const isExist = await UserModel.findOne({ email });
+  if (isExist?.email) {
+    try {
+      const hashedPassword = isExist.password;
+      bcrypt.compare(password, hashedPassword, (err, result) => {
+        if (result) {
+          const token = jwt.sign({ email: isExist.email }, "webtoken");
+          res.send({ res: `Hello ${isExist.email}`, token });
+        } else {
+          res.status(404).send({ res: "Password Incorrect" });
+        }
+      });
+    } catch (err) {
+      res.send({ res: err });
+    }
+  } else {
+    res.status(404).send({ res: "User Not Found, Try Signup First" });
+  }
 });
 
 module.exports = { loginRouter };
