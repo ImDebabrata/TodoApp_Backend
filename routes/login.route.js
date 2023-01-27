01b86510cd49212bee9090b1f7381a3bbdacbef9
ImDebabrata <debabratakgt@hotmail.com>
2023-01-28 01:21:05 +0530

    login functionallity

--- routes/login.route.js
+++ routes/login.route.js
@@ -3,7 +3,10 @@ const { UserModel } = require("../models/User.model");
 const loginRouter = express.Router();
 
 loginRouter.post("/", (req, res) => {
-  console.log("req:", req.body);
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(401).send({ res: "Please input Email and Password" });
+  }
   res.send("Login Route");
 });
 
