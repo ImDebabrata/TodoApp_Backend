6a78ff7b829731b5432725cb25cb98c903bf9405
ImDebabrata <debabratakgt@hotmail.com>
2023-02-09 20:07:45 +0530

    todo delete functionallity

--- routes/todo.route.js
+++ routes/todo.route.js
@@ -7,9 +7,9 @@ const todoRouter = express.Router();
 
 todoRouter.post("/", async (req, res) => {
   const { userID } = req.body;
-  console.log("userID:", userID);
+  // console.log("userID:", userID);
   const todos = await TodoModel.find({ userID });
-  console.log("todos:", todos);
+  // console.log("todos:", todos);
   res.send({ todos });
 });
 
@@ -17,11 +17,25 @@ todoRouter.post("/add", async (req, res) => {
   const { title, category, id, userID } = req.body;
   try {
     const todo = new TodoModel({ title, category, id, userID });
-    console.log("todo:", todo);
+    // console.log("todo:", todo);
     await todo.save();
     res.send({ res: "Added new todo", todo });
   } catch (err) {
     res.send({ res: err });
+  }
+});
+
+todoRouter.delete("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    res.status(404).send({ res: "Invalid Request" });
+    return;
+  }
+  try {
+    await TodoModel.findByIdAndDelete({ _id: id });
+    res.send({ res: "Deleted Successfully" });
+  } catch (err) {
+    res.send({ err });
   }
 });
 
